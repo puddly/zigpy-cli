cc3045d9409b52948226f4a844a09eddce6923aa
puddly <32534428+puddly@users.noreply.github.com>
2025-01-22 14:54:06 -0500

    Include `CHANNELS_LIST`

--- zigpy_cli/common.py
+++ zigpy_cli/common.py
@@ -1,4 +1,5 @@
 import click
+from zigpy.types import Channels
 
 
 class HexOrDecIntParamType(click.ParamType):
@@ -17,4 +18,18 @@ class HexOrDecIntParamType(click.ParamType):
             self.fail(f"{value!r} is not a valid integer", param, ctx)
 
 
+class ChannelsType(click.ParamType):
+    name = "channels"
+
+    def convert(self, value, param, ctx):
+        if isinstance(value, Channels):
+            return value
+
+        try:
+            return Channels.from_channel_list(map(int, value.split(",")))
+        except ValueError:
+            self.fail(f"{value!r} is not a valid channel list", param, ctx)
+
+
 HEX_OR_DEC_INT = HexOrDecIntParamType()
+CHANNELS_LIST = ChannelsType()
